8b52f71a8734e8b86683d63880f070555ab18e4d
mingray <mingray_hsu@perfectcorp.com>
2020-05-15 11:30:26 +0800

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>還敢下來阿冰鳥</div>
+    );
+  }
+}
+
+export default index;
